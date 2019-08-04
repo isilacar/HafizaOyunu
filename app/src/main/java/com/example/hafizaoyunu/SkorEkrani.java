da5cd3f6947b3e10eb70d06b37adab552934b470
Işıl Acar <isilacarisil@gmail.com>
2019-08-04 15:41:05 +0300

    04.08.2019 skorekranına tekrar oyna butonu eklendi

--- app/src/main/java/com/example/hafizaoyunu/SkorEkrani.java
+++ app/src/main/java/com/example/hafizaoyunu/SkorEkrani.java
@@ -3,12 +3,15 @@ package com.example.hafizaoyunu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class SkorEkrani extends AppCompatActivity {
 
 TextView hatat;
+Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,7 @@ TextView hatat;
         setContentView(R.layout.skorekrani);
 
         hatat=findViewById( R.id.hata);
-
+        button=findViewById(R.id.button);
 
 
         Intent i=getIntent();
@@ -25,6 +28,14 @@ TextView hatat;
         String isim=i.getStringExtra("isim");
 
         hatat.setText(isim+", "+hata + " hata ile oyunu bitirdin.Tebrikler..");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(SkorEkrani.this,OyunEkrani.class);
+                startActivity(i);
+            }
+        });
 
     }
 
